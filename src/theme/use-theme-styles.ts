97ee6fe9de153a8649d794576bf259fb3414e3ef
Remco Pander <rpander@deloitte.nl>
2022-11-23 15:01:27 +0100

    chore: fix typing

--- src/theme/use-theme-styles.ts
+++ src/theme/use-theme-styles.ts
@@ -4,9 +4,9 @@ import useTheme from "./use-theme";
 import * as theme from "./values";
 
 type Theme = typeof theme;
-type ThemeGenerator<T extends {}> = (theme: Theme) => T;
+type ThemeGenerator<T> = (theme: Theme) => T;
 
-export default function useThemeStyles<T extends {}>(fn: ThemeGenerator<T>) {
+export default function useThemeStyles<T>(fn: ThemeGenerator<T>) {
   const { theme } = useTheme();
 
   return useMemo(() => fn(theme), [theme]);
