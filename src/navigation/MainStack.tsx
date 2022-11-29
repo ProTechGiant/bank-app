17ca3d6c761600690b1b801a08d1ecd12c49b24e
ambervdam <102952220+ambervdam@users.noreply.github.com>
2022-11-29 11:33:27 +0100

    chore: changed import types

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -2,9 +2,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ApplyForCardScreen from "@/features/Cards/ApplyForCardScreen";
 import HomeDashboardScreen from "@/features/Home/HomeDashboardScreen";
-import RouteParams from "./mainStackParams";
+import MainStackParams from "./mainStackParams";
 import TemporaryLandingPage from "@/features/TemporaryLandingPage/TemporaryLandingPage";
-const Stack = createNativeStackNavigator<RouteParams>();
+const Stack = createNativeStackNavigator<MainStackParams>();
 
 export default function MainStack() {
   return (
--- src/navigation/use-navigation.ts
+++ src/navigation/use-navigation.ts
@@ -1,7 +1,7 @@
 import { NavigationProp, useNavigation as rnUseNavigation } from "@react-navigation/native";
 
-import RouteParams from "./mainStackParams";
+import MainStackParams from "./mainStackParams";
 
 export default function useNavigation() {
-  return rnUseNavigation<NavigationProp<RouteParams>>();
+  return rnUseNavigation<NavigationProp<MainStackParams>>();
 }
