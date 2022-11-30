921105418b4f25411cab95abb1d227f6baec336c
ambervdam <102952220+ambervdam@users.noreply.github.com>
2022-11-30 10:58:09 +0100

    chore: clearer folder naming

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,6 +1,6 @@
-import { default as AddIcon } from "./images/add.svg";
-import { default as SplitIcon } from "./images/split.svg";
-import { default as TransferIcon } from "./images/transfer.svg";
+import { default as AddIcon } from "./icons/add.svg";
+import { default as SplitIcon } from "./icons/split.svg";
+import { default as TransferIcon } from "./icons/transfer.svg";
 
 export const Icons = {
   Add: AddIcon,
