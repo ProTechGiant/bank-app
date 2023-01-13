e728202ad50384a8a65f8629e4f477de11471b06
Remco Pander <rpander@deloitte.nl>
2023-01-13 09:15:16 +0000

    Pull request #84: feat: add sliding in/ out animation to modal
    Merge in MOB/croatia-mobile-app from feat/modal-animation to main
    
    Squashed commit of the following:
    
    commit 3ebe710c08f412215eac4d76af9a295dc62fe6b8
    Merge: 76b5661 521ab45
    Author: Remco Pander <rpander@deloitte.nl>
    Date:   Fri Jan 13 10:14:53 2023 +0100
    
        Merge branch 'main' into feat/modal-animation
    
    commit 76b5661d43cd6a647f7832eb697da046a3ae68fe
    Author: Remco Pander <rpander@deloitte.nl>
    Date:   Thu Jan 12 16:38:57 2023 +0100
    
        feat: add sliding in/ out animation to modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,4 +1,22 @@
-import { Modal as RNModal, ModalProps as RNModalProps, Pressable, StyleSheet, View, ViewStyle } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  Modal as RNModal,
+  ModalProps as RNModalProps,
+  Pressable,
+  StyleSheet,
+  useWindowDimensions,
+  View,
+  ViewStyle,
+} from "react-native";
+import Animated, {
+  Easing,
+  interpolate,
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { CloseIcon } from "@/assets/icons";
 import Typography from "@/components/Typography";
@@ -9,16 +27,36 @@ interface ModalProps extends Omit<RNModalProps, "animationType" | "onRequestClos
   headerText: string;
 }
 
-export default function Modal({ children, onClose, headerText, ...nativeModalProps }: ModalProps) {
+export default function Modal({ children, onClose, headerText, visible = false, ...nativeModalProps }: ModalProps) {
+  const insets = useSafeAreaInsets();
+  const { height } = useWindowDimensions();
+
+  const contentHeight = useSharedValue<number | undefined>(height);
+  const transitionPhase = useSharedValue(visible ? 1 : 0);
+  const [isVisible, setIsVisible] = useState(visible);
+
+  useEffect(() => {
+    if (visible === isVisible) return;
+
+    if (visible) {
+      setIsVisible(true);
+    }
+
+    if (!visible) {
+      handleStartTransitioning();
+      setTimeout(() => setIsVisible(false), MOVE_OUT_DURATION_MS + 25);
+    }
+  }, [visible]);
+
   const containerStyles = useThemeStyles<ViewStyle>(
     theme => ({
       backgroundColor: theme.palette["neutralBase-50"],
       borderTopLeftRadius: theme.radii.small * 1.5,
       borderTopRightRadius: theme.radii.small * 1.5,
-      height: "43%",
       marginTop: "auto",
+      paddingBottom: insets.bottom,
     }),
-    []
+    [insets.bottom]
   );
 
   const headerStyles = useThemeStyles<ViewStyle>(
@@ -40,10 +78,34 @@ export default function Modal({ children, onClose, headerText, ...nativeModalPro
     []
   );
 
+  const containerAnimatedStyles = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateY: interpolate(transitionPhase.value, [0, 1], [contentHeight.value ?? 0, 0]),
+      },
+    ],
+  }));
+
+  const backdropAnimatedStyles = useAnimatedStyle(() => ({
+    backgroundColor: interpolateColor(transitionPhase.value, [0, 1], ["rgba(0, 0, 0, 0)", "rgba(0, 0, 0, 0.6)"]),
+  }));
+
+  const handleStartTransitioning = () => {
+    transitionPhase.value = withTiming(visible ? 1 : 0, {
+      easing: Easing.bezier(0.42, 0, 0.58, 1),
+      duration: visible ? MOVE_IN_DURATION_MS : MOVE_OUT_DURATION_MS,
+    });
+  };
+
   return (
-    <RNModal onRequestClose={onClose} transparent {...nativeModalProps}>
-      <Pressable onPress={onClose} style={styles.backdrop} />
-      <View style={containerStyles}>
+    <RNModal onRequestClose={onClose} transparent visible={isVisible} {...nativeModalProps}>
+      <Animated.View onTouchStart={() => onClose?.()} style={[styles.backdrop, backdropAnimatedStyles]} />
+      <Animated.View
+        onLayout={event => {
+          contentHeight.value = event.nativeEvent.layout.height;
+          if (visible) handleStartTransitioning();
+        }}
+        style={[containerStyles, containerAnimatedStyles]}>
         <View style={headerStyles}>
           <View style={horizontalSpacing} />
           <View style={styles.headerTextContainer}>
@@ -60,14 +122,13 @@ export default function Modal({ children, onClose, headerText, ...nativeModalPro
           </View>
         </View>
         <View style={horizontalSpacing}>{children}</View>
-      </View>
+      </Animated.View>
     </RNModal>
   );
 }
 
 const styles = StyleSheet.create({
   backdrop: {
-    backgroundColor: "rgba(0, 0, 0, 0.6)",
     bottom: 0,
     left: 0,
     position: "absolute",
@@ -81,3 +142,5 @@ const styles = StyleSheet.create({
 });
 
 const HIT_SLOP_RIGHT = { top: 10, bottom: 10, left: 0, right: 10 };
+const MOVE_IN_DURATION_MS = 300;
+const MOVE_OUT_DURATION_MS = 150;
