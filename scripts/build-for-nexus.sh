863e48c8a1748f5434da63e59d91b06c41a5c960
Remco Pander <rpander@deloitte.nl>
2023-01-19 14:52:46 +0000

    Pull request #108: chore: adds changelog to nexus
    Merge in MOB/croatia-mobile-app from chore/upload-changelog to main
    
    Squashed commit of the following:
    
    commit d9b49e8e1cf16aa4d006bc203b4fdd2df3f66f40
    Merge: 384e648 0faa311
    Author: Remco Pander <rpander@deloitte.nl>
    Date:   Thu Jan 19 15:52:24 2023 +0100
    
        Merge branch 'main' into chore/upload-changelog
    
    commit 384e6489614d210d7425bbfe51096514db540aad
    Author: Remco Pander <rpander@deloitte.nl>
    Date:   Thu Jan 19 15:08:21 2023 +0100
    
        chore: adds changelog to nexus

--- scripts/build-for-nexus.sh
+++ scripts/build-for-nexus.sh
@@ -1,7 +1,8 @@
 PREV_BUILD_TAG=$(git tag -l --sort=-version:refname "builds/test/*" | head -1)
 PREV_BUILD_TAG_END_IDX=$(echo $PREV_BUILD_TAG | grep -b -o - | cut -d: -f1)
-
 PREV_BUILD_NUMBER=$(echo $PREV_BUILD_TAG | cut -c "13-$PREV_BUILD_TAG_END_IDX")
+
+# Need to be exported: used in Fastlane
 export BUILD_NUMBER=$((PREV_BUILD_NUMBER + 1))
 export BUILD_ENVIRONMENT="test"
 
@@ -13,7 +14,7 @@ git reset --hard
 
 # Install/ update dependencies
 yarn install
-npx pod-install
+# npx pod-install --> disabled for now, only required for Android
 bundle install
 
 # Build Android version
@@ -21,7 +22,7 @@ bundle exec fastlane update_version
 bundle exec fastlane android build "env:$BUILD_ENVIRONMENT"
 bundle exec fastlane tag_build_git
 
-# Upload to Nexus
+# Upload Android build to Nexus
 BUILD_TIME="$(date +'%Y%m%d%H%M')"
 ANDROID_INPUT_NAME="app-accept-release.apk"
 ANDROID_OUTPUT_NAME="release-$BUILD_TIME-b$BUILD_NUMBER.apk"
@@ -31,3 +32,15 @@ curl -v -u mobile:c7oY08v3hU8F -X POST "https://nexus.projectcroatia.cloud/servi
   -F asset0.filename="$ANDROID_OUTPUT_NAME" \
   -F directory="android-$BUILD_ENVIRONMENT" \
   -F asset0="@fastlane/builds/test/$ANDROID_INPUT_NAME"
+
+# Create changelog and upload to Nexus
+RELEASENOTES_FILE_NAME="release-$BUILD_TIME-b$BUILD_NUMBER.txt"
+
+echo "Changes since previous release: \n" >> $RELEASENOTES_FILE_NAME
+git log --pretty=format:"(%cs) %<(15)%aN %s" $PREV_BUILD_TAG..HEAD >> $RELEASENOTES_FILE_NAME
+
+curl -v -u mobile:c7oY08v3hU8F -X POST "https://nexus.projectcroatia.cloud/service/rest/internal/ui/upload/ios-builds" \
+  -F asset0="C:\fakepath\release-notes.txt" \
+  -F asset0.filename="$RELEASENOTES_FILE_NAME" \
+  -F directory="android-$BUILD_ENVIRONMENT" \
+  -F asset0="@$RELEASENOTES_FILE_NAME"
