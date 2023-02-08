d03c14e85f4a63470a255ab6190096b4a3bad47b
Sian Finlay <svfinlay@deloitte.co.uk>
2023-02-08 16:34:15 +0000

    Pull request #188: fix(phone-number): added missing 2 in regex
    Merge in MOB/croatia-mobile-app from fix/missing-2-in-phone-number to main
    
    Squashed commit of the following:
    
    commit 05d965e7fe22d59bd9cf0d64568f517e71e04c75
    Author: Sian Finlay <svfinlay@deloitte.co.uk>
    Date:   Wed Feb 8 16:18:26 2023 +0000
    
        fix(phone-number): added missing 2 in regex

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,7 +10,7 @@ export const alphaNumericSpecialCharsRegExp = /^[a-zA-Z0-9-’/`~!#*$@_%+=.,^&()
 
 export const numericRegExp = /^[0-9]+$/;
 
-export const saudiPhoneRegExp = /^(009665|9665|\+9665|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$/;
+export const saudiPhoneRegExp = /^(009665|9665|\+9665|05|5)(5|0|2|3|6|4|9|1|8|7)([0-9]{7})$/;
 
 export function isKeyOfObject<T extends object>(key: string | number | symbol, obj: T): key is keyof T {
   return key in obj;
