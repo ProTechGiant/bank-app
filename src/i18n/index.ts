32aa1a8c4716e93a69c9645c37a2d172f5cb3335
Remco Pander <rpander@deloitte.nl>
2023-02-20 15:35:28 +0000

    Pull request #241: chore(3754): update copy
    Merge in MOB/croatia-mobile-app from chore/3754-update-copy to main
    
    Squashed commit of the following:
    
    commit 25947699a095c8f359fd63244a9328812f6843e5
    Merge: 3abd613 93d4706
    Author: Remco Pander <rpander@deloitte.nl>
    Date:   Mon Feb 20 16:35:12 2023 +0100
    
        Merge remote-tracking branch 'origin/main' into chore/3754-update-copy
    
    commit 3abd613e83aff98b5bafe8254346d3998f8ec33c
    Author: Remco Pander <rpander@deloitte.nl>
    Date:   Mon Feb 20 15:22:47 2023 +0100
    
        chore(3754): update copy

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -464,7 +464,7 @@ export const resources = {
             helperIfDayExceeds28:
               "Transfer will be made in the last day of the month if selected date don’t exists in the current month.",
             helperIfDayIsAfterTarget:
-              "To reach your goal your recurring payments need to happen before your target date.",
+              "You have selected a date after your target goal date. You should select a date before your target goal date to reach your goal.",
             recurringDeposit: {
               title: "Recurring deposit",
               modalTitle: "You have created a recurring deposit",
