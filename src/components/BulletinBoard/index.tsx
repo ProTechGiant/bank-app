28bf2c279b496f8bd8a55adae149706d96568f73
Amber Van Dam <avandam@deloitte.nl>
2023-02-27 09:23:46 +0000

    Pull request #261: fix: react hook error bulletin board
    Merge in MOB/croatia-mobile-app from fix/bulletin-board-react-hook-error to main
    
    Squashed commit of the following:
    
    commit 094aa3f1d2af133b8aee2c21d4d5766a51c98dcd
    Author: ambervdam <avandam@deloitte.nl>
    Date:   Mon Feb 27 09:12:50 2023 +0100
    
        fix: fixed error on render
    
    commit 4f092e5008f0c65c5293ff3f358e10bd2e1e7fe0
    Author: ambervdam <avandam@deloitte.nl>
    Date:   Fri Feb 24 14:27:12 2023 +0100
    
        fix: react hook error bulletin board
    
    commit aa3b5d4e987331ab0c5e152a58ff1322906f5208
    Author: ambervdam <avandam@deloitte.nl>
    Date:   Fri Feb 24 14:04:27 2023 +0100
    
        fix: react hook error bulletin board

--- src/components/BulletinBoard/index.tsx
+++ src/components/BulletinBoard/index.tsx
@@ -16,15 +16,17 @@ export default function BulletinBoard<T>({ data, title }: BulletinBoardProps<T>)
   const carouselWidth = useThemeStyles<number>(theme => Dimensions.get("screen").width - theme.spacing["16p"] * 2, []);
   const [isExpanded, setIsExpanded] = useState(false);
 
+  const container = useThemeStyles<ViewStyle>(theme => ({
+    backgroundColor: theme.palette["neutralBase-50"],
+  }));
+
   const handleOnToggle = () => {
     setIsExpanded(!isExpanded);
   };
 
-  if (data.length === 0) return null;
-
-  const container = useThemeStyles<ViewStyle>(theme => ({
-    backgroundColor: theme.palette["neutralBase-50"],
-  }));
+  if (!data || !data.length) {
+    return <></>;
+  }
 
   return (
     <View style={container}>
