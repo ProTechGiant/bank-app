7b7ec0d829317ef1ba201628c993afdd32a4255e
Carmen Chan <carmenchan@deloitte.co.uk>
2023-04-11 07:50:15 +0000

    Pull request #484: chore: pc-8691 update toggle color
    Merge in MOB/croatia-mobile-app from chore/PC-8691-toggle-branding-color to main
    
    Squashed commit of the following:
    
    commit 1d7c6eb59ca3a2c0f4b4e49ec1cee7af79d3ce68
    Author: Carmen Chan <carmen.chan@deloitte.co.uk>
    Date:   Thu Apr 6 15:49:01 2023 +0100
    
        chore: pc-8691 update toggle color

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -11,15 +11,25 @@ interface ToggleProps {
 }
 
 export default function Toggle({ onPress, testID, disabled, value }: ToggleProps) {
-  const disabledOffColor = useThemeStyles<string>(theme => theme.palette["neutralBase-20-30%"]);
-  const offColor = useThemeStyles<string>(theme => theme.palette["neutralBase-20"]);
+  const disabledOffColor = useThemeStyles<string>(theme => theme.palette["neutralBase-40"]);
+  const offColor = useThemeStyles<string>(theme => theme.palette["neutralBase-30"]);
   const onColor = useThemeStyles<string>(theme => theme.palette.complimentBase);
 
   const buttonStyle = useThemeStyles<ViewStyle>(theme => ({
     borderRadius: BUTTON_SIZE / 2,
     height: BUTTON_SIZE,
     width: BUTTON_SIZE,
-    backgroundColor: theme.palette["neutralBase-50"],
+    backgroundColor: theme.palette["neutralBase-60"],
+    borderWidth: 0.5,
+    borderColor: "#0000000D",
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.16,
+    shadowRadius: 4,
+    elevation: 1,
   }));
 
   const containerStyle = useThemeStyles<ViewStyle>(() => ({
