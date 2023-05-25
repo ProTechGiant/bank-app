79f24d601d4bb81dba8e13f0f6856e228eb5a30a
Remco Pander <rpander@deloitte.nl>
2023-05-25 11:37:15 +0000

    Pull request #728: fix: issue on iOS with import of `DatePickerIOS`
    Merge in MOB/croatia-mobile-app from fix/ios-datepickerios to main
    
    Squashed commit of the following:
    
    commit ee23ba138ef666673b6bafbcc477db531a2a7e6b
    Merge: 34bbf5d7 a150b9d2
    Author: Remco Pander <rpander@deloitte.nl>
    Date:   Thu May 25 13:37:00 2023 +0200
    
        Merge remote-tracking branch 'origin/main' into fix/ios-datepickerios
    
    commit 34bbf5d71c94179e00d4e81248e125b26c81fa7a
    Author: Remco Pander <rpander@deloitte.nl>
    Date:   Thu May 25 13:10:58 2023 +0200
    
        fix: issue on iOS with import of `DatePickerIOS`

--- metro.config.js
+++ metro.config.js
@@ -1,27 +1,35 @@
-const { getDefaultConfig } = require("metro-config");
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-env node */
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-};
+const { getDefaultConfig } = require("metro-config");
 
 module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
+  // eslint-disable-next-line prettier/prettier
+  const { resolver: { sourceExts, assetExts } } = await getDefaultConfig();
+
   return {
     transformer: {
       babelTransformerPath: require.resolve("react-native-svg-transformer"),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
     },
     resolver: {
       assetExts: assetExts.filter(ext => ext !== "svg"),
       sourceExts: [...sourceExts, "svg"],
+    },
+    // see https://github.com/facebook/react-native/issues/36794#issuecomment-1500880284
+    server: {
+      rewriteRequestUrl: url => {
+        if (!url.endsWith(".bundle")) {
+          return url;
+        }
+
+        // JavaScriptCore strips query strings, so try to re-add them with a best guess.
+        return url + "?platform=ios&dev=true&minify=false&modulesOnly=false&runModule=true";
+      },
     },
   };
 })();
