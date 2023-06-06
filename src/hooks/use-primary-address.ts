9445769ff40fa3b6a0b9ddd22a43c02016823575
Fatos Varol <fvarol@deloitte.nl>
2023-06-06 15:01:40 +0000

    Pull request #755: fix: update useprimaryaddress hook
    Merge in MOB/croatia-mobile-app from fix/update-use-primary-address-hook to main
    
    Squashed commit of the following:
    
    commit b27ebbff59862b1c5fc0e953519f4769f88e9180
    Author: Fatos Varol <fvarol@deloitte.nl>
    Date:   Tue Jun 6 16:30:43 2023 +0200
    
        fix: update useprimaryaddress hook

--- src/hooks/use-primary-address.ts
+++ src/hooks/use-primary-address.ts
@@ -2,9 +2,12 @@ import { useQuery } from "react-query";
 
 import api from "@/api";
 import { Address } from "@/types/Address";
+import { generateRandomId } from "@/utils";
 
 export default function usePrimaryAddress() {
   return useQuery(["cards", "primary-address"], () => {
-    return api<Address>("v1", "cards/customer/address", "GET");
+    return api<Address>("v1", "cards/customer/address", "GET", undefined, undefined, {
+      ["x-correlation-id"]: generateRandomId(),
+    });
   });
 }
