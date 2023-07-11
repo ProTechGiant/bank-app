d5bda9c059575d075543ef4901757fc649b99376
Khaled Alaa Eldin <kalaaeldin@ejada.com>
2023-07-11 10:22:05 +0000

    Pull request #852: Fix(PC-12746) fix header shrink upon scrolling home
    Merge in MOB/croatia-mobile-app from fix/12746-fix-header-shrink-upon-scrolling-home to main
    
    Squashed commit of the following:
    
    commit 074463bab9040083ad6f0cb607680b196e61605c
    Merge: 570379be5 6a32eeb94
    Author: Khaled Alaa Eldin <khaaeldin@ejada.com>
    Date:   Tue Jul 11 13:20:13 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into fix/12746-fix-header-shrink-upon-scrolling-home
    
    commit 570379be56cd6a0f8302862bf1f2bef7fe678720
    Merge: bdebda15f d3e657a47
    Author: Khaled Alaa Eldin <khaaeldin@ejada.com>
    Date:   Tue Jul 11 13:05:42 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into fix/12746-fix-header-shrink-upon-scrolling-home
    
    commit bdebda15ff6525c806072a71677f973fc48e2fb2
    Merge: dd04f7917 b65e350da
    Author: Khaled Alaa Eldin <khaaeldin@ejada.com>
    Date:   Mon Jul 10 21:43:43 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into fix/12746-fix-header-shrink-upon-scrolling-home
    
    commit dd04f79175c782f5f387214e8bfc5ad7c7e17fe2
    Author: Khaled Alaa Eldin <khaaeldin@ejada.com>
    Date:   Mon Jul 10 21:43:00 2023 +0300
    
        fix: use useAnimatedScrollHandler  with scroll
    
    commit 9c27338903159e58e580faf434fb867fb214a090
    Merge: 9598967dd 065b80366
    Author: Khaled Alaa Eldin <khaaeldin@ejada.com>
    Date:   Mon Jul 10 13:53:35 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into fix/12746-fix-header-shrink-upon-scrolling-home
    
    commit 9598967dd36337545238bcb6bf375a40ba7d25b1
    Author: Khaled Alaa Eldin <khaaeldin@ejada.com>
    Date:   Mon Jul 10 13:52:47 2023 +0300
    
        fix: handle antimation with another way
    
    commit 49d7ab7a79b623d7a57920a40ae8aaf106341187
    Merge: 043a87160 ef74e6f50
    Author: Khaled Alaa Eldin <khaaeldin@ejada.com>
    Date:   Sat Jul 8 09:36:13 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into fix/12746-fix-header-shrink-upon-scrolling-home
    
    commit 043a8716027343c60932f17385441e602061325e
    Author: Khaled Alaa Eldin <khaaeldin@ejada.com>
    Date:   Fri Jul 7 17:06:50 2023 +0300
    
        fix: change function name  expand-collaps onpress
    
    commit 6f1ec4ebb64db4ad20e2bed79f7ede48aac750ad
    Merge: a721800e7 0205c3b92
    Author: Khaled Alaa Eldin <khaaeldin@ejada.com>
    Date:   Fri Jul 7 16:58:53 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into fix/12746-fix-header-shrink-upon-scrolling-home
    
    commit a721800e736bfba97bc301eae10ef4edda558350
    Author: Khaled Alaa Eldin <khaaeldin@ejada.com>
    Date:   Fri Jul 7 16:58:25 2023 +0300
    
        fix: enhance animated shrink function in home page
    
    commit 46d7980e07d7169cb43ec59e930409a092fc90a0
    Merge: 7414ddfd1 16049af5b
    Author: Khaled Alaa Eldin <khaaeldin@ejada.com>
    Date:   Thu Jul 6 15:21:17 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into fix/12746-fix-header-shrink-upon-scrolling-home
    
    commit 7414ddfd15e60a4baca262e51e396071d6be545d
    Author: Khaled Alaa Eldin <khaaeldin@ejada.com>
    Date:   Thu Jul 6 15:20:36 2023 +0300
    
        fix: animated shrink header homeScreen

--- src/features/Home/screens/DashboardScreen.tsx
+++ src/features/Home/screens/DashboardScreen.tsx
@@ -1,7 +1,15 @@
 import Clipboard from "@react-native-clipboard/clipboard";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Pressable, ScrollView, StatusBar, StyleSheet, View, ViewStyle } from "react-native";
+import { Pressable, StatusBar, StyleSheet, TextStyle, View, ViewStyle } from "react-native";
+import Animated, {
+  runOnJS,
+  useAnimatedReaction,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { AccountIcon, HideIcon, ShowIcon, UserClockIcon } from "@/assets/icons";
@@ -37,12 +45,46 @@ export default function DashboardScreen() {
   const [isBalanceVisible, setIsBalanceVisible] = useState(true);
   const [isNotificationsExpanded, setIsNotificationsExpanded] = useState(false);
   const [layoutErrorIsVisible, setLayoutErrorIsVisible] = useState(false);
+  const [headerShrinkStatus, setHeaderShrinkStatus] = useState(false);
+  const isNotificationsExpandedSharedValue = useSharedValue(false);
+  const headerShrinkStatusSharedValue = useSharedValue(false);
+  const headerHeight = useSharedValue(305);
+  const accountBalanceFont = useSharedValue(42);
+  const currencyFont = useSharedValue(16);
 
   useEffect(() => {
     if (homepageLayout?.isError === true) {
       setLayoutErrorIsVisible(true);
     }
   }, [homepageLayout]);
+
+  const handleScroll = useAnimatedScrollHandler(event => {
+    if (isNotificationsExpandedSharedValue.value) isNotificationsExpandedSharedValue.value = false;
+    const offsetY = Math.abs(event.contentOffset.y);
+    const headerHeightUpdatedValue = offsetY > 0 ? 180 : 305;
+    const accountBalanceFontValue = offsetY > 0 ? 18 : 42;
+    const currencyFontValue = offsetY > 0 ? 16 : 16;
+    if (offsetY !== 0 && !headerShrinkStatusSharedValue.value) headerShrinkStatusSharedValue.value = true;
+    else if (offsetY === 0 && headerShrinkStatusSharedValue.value) headerShrinkStatusSharedValue.value = false;
+    headerHeight.value = withSpring(headerHeightUpdatedValue);
+    currencyFont.value = withSpring(currencyFontValue);
+    accountBalanceFont.value = withSpring(accountBalanceFontValue);
+  });
+
+  const updateNotificationsExpanded = useCallback((newValue: boolean) => setIsNotificationsExpanded(newValue), []);
+  const updateheaderShrinkStatus = useCallback((newValue: boolean) => setHeaderShrinkStatus(newValue), []);
+
+  useAnimatedReaction(
+    () => isNotificationsExpandedSharedValue.value,
+    value => runOnJS(updateNotificationsExpanded)(value),
+    [isNotificationsExpandedSharedValue.value]
+  );
+
+  useAnimatedReaction(
+    () => headerShrinkStatusSharedValue.value,
+    value => runOnJS(updateheaderShrinkStatus)(value),
+    [headerShrinkStatusSharedValue.value]
+  );
 
   const handleOnIbanCopyPress = () => {
     if (undefined === account.data?.iban) return;
@@ -86,17 +128,34 @@ export default function DashboardScreen() {
     handleOnLoadingErrorClose();
   };
 
+  const onExpandCollapsPress = (value: boolean) => {
+    headerHeight.value = withSpring(
+      headerShrinkStatusSharedValue.value
+        ? isNotificationsExpandedSharedValue.value
+          ? 180
+          : 305
+        : isNotificationsExpandedSharedValue.value
+        ? 305
+        : 475
+    );
+    isNotificationsExpandedSharedValue.value = value;
+  };
+
   const headerStyle = useThemeStyles<ViewStyle>(theme => ({
     paddingHorizontal: theme.spacing["20p"],
     paddingTop: theme.spacing["12p"],
     paddingBottom: theme.spacing["12p"],
+    overflow: "hidden",
   }));
 
-  const headerBalanceStyle = useThemeStyles<ViewStyle>(theme => ({
-    alignItems: "baseline",
-    flexDirection: "row",
-    paddingTop: theme.spacing["24p"],
-  }));
+  const headerBalanceStyle = useThemeStyles<ViewStyle>(
+    theme => ({
+      alignItems: "baseline",
+      flexDirection: "row",
+      paddingTop: !headerShrinkStatus ? theme.spacing["24p"] : 0,
+    }),
+    [headerShrinkStatus]
+  );
 
   const headerButtonsStyle = useThemeStyles<ViewStyle>(theme => ({
     marginTop: theme.spacing["12p"],
@@ -112,41 +171,84 @@ export default function DashboardScreen() {
     paddingTop: theme.spacing["20p"],
   }));
 
+  const accountBalanceTextStyle = useThemeStyles<TextStyle>(
+    theme => ({
+      color: theme.palette["neutralBase+30"],
+      fontWeight: theme.typography.text.weights.semiBold,
+      marginBottom: headerShrinkStatus ? theme.spacing["8p"] : 0,
+    }),
+    [headerShrinkStatus]
+  );
+
+  const currencyTextStyle = useThemeStyles<TextStyle>(
+    theme => ({
+      color: theme.palette["neutralBase+30"],
+      fontWeight: theme.typography.text.weights.regular,
+      marginBottom: headerShrinkStatus ? theme.spacing["8p"] : 0,
+    }),
+    [headerShrinkStatus]
+  );
+
+  const animatedhearderHeightStyles = useAnimatedStyle(() => {
+    return {
+      height: headerHeight.value,
+    };
+  });
+
+  const animatedaccountBalanceFontStyles = useAnimatedStyle(() => {
+    return {
+      fontSize: accountBalanceFont.value,
+    };
+  });
+
+  const animatedcurrencyFontStyles = useAnimatedStyle(() => {
+    return {
+      height: currencyFont.value,
+    };
+  });
+
   return (
-    <>
-      <Page backgroundColor="neutralBase-60" insets={["left", "right", "bottom"]}>
-        <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
-        <View style={styles.backgroundImage}>
-          {/* Ideally we do this animated. Not sure (yet) how we can do that though.. */}
-          {isNotificationsExpanded ? <BackgroundExpandedSvg /> : <BackgroundCollapsedSvg />}
-        </View>
-        <SafeAreaView edges={["top"]} style={styles.container}>
-          <View style={headerStyle}>
-            <View style={styles.headerCentered}>
-              <Typography.Text color="neutralBase+30" size="footnote" weight="medium" style={styles.headerText}>
-                {account.data?.name ?? "-"}
-              </Typography.Text>
+    <Page backgroundColor="neutralBase-60" insets={["left", "right", "bottom"]}>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Animated.View style={[styles.backgroundImage, animatedhearderHeightStyles]}>
+        {headerShrinkStatus ? (
+          <BackgroundCollapsedSvg />
+        ) : isNotificationsExpanded ? (
+          <BackgroundExpandedSvg />
+        ) : (
+          <BackgroundCollapsedSvg />
+        )}
+      </Animated.View>
+      <SafeAreaView edges={["top"]} style={styles.container}>
+        <Animated.View style={[headerStyle, animatedhearderHeightStyles]}>
+          <View style={styles.headerCentered}>
+            <Typography.Text color="neutralBase+30" size="footnote" weight="medium" style={styles.headerText}>
+              {account.data?.name ?? "-"}
+            </Typography.Text>
+            {!headerShrinkStatus && (
               <Pressable onPress={handleOnIbanCopyPress}>
                 <Typography.Text color="neutralBase+30" size="footnote" weight="medium" style={styles.headerText}>
                   {account.data?.iban ?? "-"}
                 </Typography.Text>
               </Pressable>
-              <View style={headerBalanceStyle}>
-                {isBalanceVisible ? (
-                  <>
-                    <Typography.Header color="neutralBase+30" size="large" weight="semiBold">
-                      {formatCurrency(account.data?.balance ?? 0)}
-                    </Typography.Header>
-                    <Typography.Text color="neutralBase+30" size="footnote" weight="regular">
-                      {" " + (account.data?.currencyType ?? "SAR")}
-                    </Typography.Text>
-                  </>
-                ) : (
-                  <Typography.Text color="neutralBase+30" size="body" weight="regular">
-                    {t("Home.DashboardScreen.balanceHidden")}
-                  </Typography.Text>
-                )}
-              </View>
+            )}
+            <View style={headerBalanceStyle}>
+              {isBalanceVisible ? (
+                <>
+                  <Animated.Text style={[accountBalanceTextStyle, animatedaccountBalanceFontStyles]}>
+                    {formatCurrency(account.data?.balance ?? 0)}
+                  </Animated.Text>
+                  <Animated.Text style={[currencyTextStyle, animatedcurrencyFontStyles]}>
+                    {" " + (account.data?.currencyType ?? "SAR")}
+                  </Animated.Text>
+                </>
+              ) : (
+                <Typography.Text color="neutralBase+30" size="body" weight="regular">
+                  {t("Home.DashboardScreen.balanceHidden")}
+                </Typography.Text>
+              )}
+            </View>
+            {!headerShrinkStatus && (
               <Stack direction="horizontal" gap="8p" style={headerButtonsStyle}>
                 <HeaderButton
                   onPress={() => setIsBalanceVisible(c => !c)}
@@ -159,61 +261,62 @@ export default function DashboardScreen() {
                   {t("Home.DashboardScreen.myAccountButton")}
                 </HeaderButton>
               </Stack>
-            </View>
-            <BulletinBoard
-              isExpanded={isNotificationsExpanded}
-              onExpandPress={value => setIsNotificationsExpanded(value)}
-              iconStart={<UserClockIcon />}
-              title={t("Home.DashboardScreen.notifications", { count: notifications.length })}>
-              {notifications.map(notification => (
-                <NotificationSlide
-                  key={notification.action_id}
-                  onPress={handleOnNotificationPress}
-                  notification={notification}
-                />
-              ))}
-            </BulletinBoard>
+            )}
           </View>
-          <ScrollView contentContainerStyle={contentStyle}>
-            <Stack align="stretch" direction="vertical" gap="32p">
-              {sections?.length !== 0 ? (
-                <>
-                  {sections.map(section => {
-                    if (section.type === "quick-actions") {
-                      return <QuickActionsSection key={section.type} onViewAllPress={handleOnShortcutsPress} />;
-                    }
-                    if (section.type === "rewards") {
-                      return <RewardsSection key={section.type} onViewAllPress={handleOnRewardsPress} />;
-                    }
-                    if (section.type === "articles") {
-                      return <WhatsNextSection key={section.type} onViewAllPress={handleOnWhatsNextPress} />;
-                    }
-                    return <Fragment key={section.type} />;
-                  })}
-                  <View style={footerButtonStyle}>
-                    <Button onPress={handleOnEditPress} variant="tertiary">
-                      {t("Home.DashboardScreen.editDashboard")}
-                    </Button>
-                  </View>
-                </>
-              ) : layoutErrorIsVisible === true ? (
-                <LoadingErrorNotification
-                  isVisible={layoutErrorIsVisible}
-                  onClose={handleOnLoadingErrorClose}
-                  onRefresh={handleOnLoadingErrorRefresh}
-                />
-              ) : null}
-            </Stack>
-          </ScrollView>
-        </SafeAreaView>
-      </Page>
-    </>
+          <BulletinBoard
+            isExpanded={isNotificationsExpanded}
+            onExpandPress={onExpandCollapsPress}
+            iconStart={<UserClockIcon />}
+            title={t("Home.DashboardScreen.notifications", { count: notifications.length })}>
+            {notifications.map(notification => (
+              <NotificationSlide
+                key={notification.action_id}
+                onPress={handleOnNotificationPress}
+                notification={notification}
+              />
+            ))}
+          </BulletinBoard>
+        </Animated.View>
+        <Animated.ScrollView contentContainerStyle={contentStyle} scrollEventThrottle={16} onScroll={handleScroll}>
+          <Stack align="stretch" direction="vertical" gap="32p">
+            {sections?.length !== 0 ? (
+              <>
+                {sections.map(section => {
+                  if (section.type === "quick-actions") {
+                    return <QuickActionsSection key={section.type} onViewAllPress={handleOnShortcutsPress} />;
+                  }
+                  if (section.type === "rewards") {
+                    return <RewardsSection key={section.type} onViewAllPress={handleOnRewardsPress} />;
+                  }
+                  if (section.type === "articles") {
+                    return <WhatsNextSection key={section.type} onViewAllPress={handleOnWhatsNextPress} />;
+                  }
+                  return <Fragment key={section.type} />;
+                })}
+                <View style={footerButtonStyle}>
+                  <Button onPress={handleOnEditPress} variant="tertiary">
+                    {t("Home.DashboardScreen.editDashboard")}
+                  </Button>
+                </View>
+              </>
+            ) : layoutErrorIsVisible === true ? (
+              <LoadingErrorNotification
+                isVisible={layoutErrorIsVisible}
+                onClose={handleOnLoadingErrorClose}
+                onRefresh={handleOnLoadingErrorRefresh}
+              />
+            ) : null}
+          </Stack>
+        </Animated.ScrollView>
+      </SafeAreaView>
+    </Page>
   );
 }
 
 const styles = StyleSheet.create({
   backgroundImage: {
     ...StyleSheet.absoluteFillObject,
+    overflow: "hidden",
   },
   container: {
     flex: 1,
