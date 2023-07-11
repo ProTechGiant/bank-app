d3e657a4746c8c71c73ded022fe97d15dda4b6ed
Abdul Kawee <malik-v@ejada.com>
2023-07-11 09:57:30 +0000

    Pull request #875: Fix(PC-13618) handled case where beneficiary was not of ARB
    Merge in MOB/croatia-mobile-app from fix/PC-13618-benef-not-of-ARB to main
    
    Squashed commit of the following:
    
    commit 4a63d67b1d143ab7ce676302f81fecbe78a43b8e
    Author: Abdul Kawee <malik-v@ejada.com>
    Date:   Tue Jul 11 12:28:50 2023 +0500
    
        fix: handled case where beneficiary was not of ARB

--- src/features/InternalTransfers/screens/EnterBeneficiaryDetailsScreen.tsx
+++ src/features/InternalTransfers/screens/EnterBeneficiaryDetailsScreen.tsx
@@ -106,6 +106,11 @@ export default function EnterBeneficiaryDetailsScreen() {
           ? setI18nKey("ibanForm.ibanInUseModal")
           : setI18nKey("mobileNumberForm.mobileInUseModal");
         setIsInUseErrorModalVisible(true);
+      } else if (error instanceof ApiError && error.errorContent.Message.includes(ERROR_BENEFICIARY_NOT_OF_ARB)) {
+        values.SelectionType === "accountId"
+          ? setI18nKey("accountNumberForm.accountNumberNotRecognisedModal")
+          : setI18nKey("ibanForm.ibanNotRecognisedModal");
+        setIsErrorMessageModalVisible(true);
       } else {
         setIsGenericErrorModalVisible(true);
       }
@@ -248,3 +253,4 @@ const styles = StyleSheet.create({
 
 const ERROR_BENEFICIARY_EXISTS = "beneficiary already exists";
 const ERROR_ACCOUNT_DOES_NOT_EXIST = "Account does not exist";
+const ERROR_BENEFICIARY_NOT_OF_ARB = "doesn't match Bank - ARB";
