43992678074f54f91852f2ff1a752cd344d4e62b
anofal <anofal@ejada.com>
2023-07-13 15:03:44 +0000

    Pull request #894: fix: fix issue saving-pots-err-on-landing-page
    Merge in MOB/croatia-mobile-app from fix/saving-pots-err-on-landing-page to main
    
    Squashed commit of the following:
    
    commit b68388acefe7e3ef21eb5c047069f7936323048d
    Merge: 13889c531 c61cf1327
    Author: Hadi Nofal <anofal@ejada.com>
    Date:   Thu Jul 13 18:03:22 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into fix/saving-pots-err-on-landing-page
    
    commit 13889c531c954ba24f9b42a3ce4ea8c5a70b49da
    Merge: bb0db7629 80abb91e9
    Author: Hadi Nofal <anofal@ejada.com>
    Date:   Thu Jul 13 17:19:17 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into fix/saving-pots-err-on-landing-page
    
    commit bb0db762905cc0d31d1b0f467a64231977fa88c6
    Author: Hadi Nofal <anofal@ejada.com>
    Date:   Thu Jul 13 13:52:54 2023 +0300
    
        fix: fix issue

--- src/features/Temporary/use-savings-goal-number.ts
+++ src/features/Temporary/use-savings-goal-number.ts
@@ -11,6 +11,7 @@ export default function useSavingsGoalNumber() {
   return useMutation(() => {
     return api<SavingsGoalNumResponse>("v1", "customers/savings-pots/check-no", "GET", undefined, undefined, {
       ["x-Correlation-Id"]: generateRandomId(),
+      UserId: "301", // replace with appropriate user id
     });
   });
 }
