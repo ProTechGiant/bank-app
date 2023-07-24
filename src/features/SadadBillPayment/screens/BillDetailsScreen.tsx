4f10dc3a99ef86b755e1d5ae1be193390f721d4c
Abbas Raza <abbas-v@ejada.com>
2023-07-24 14:41:42 +0000

    Pull request #940: feat(PC-2447): delete sadad bill
    Merge in MOB/croatia-mobile-app from feat/PC-2447-delete-sadad-bill to main
    
    Squashed commit of the following:
    
    commit 397a885d411ce82d1a4c21258c938a6c18ed288a
    Author: Abbas Raza Jafri <abbas-v@ejada.com>
    Date:   Mon Jul 24 12:35:01 2023 +0500
    
        feat: delete sadad bill

--- src/features/SadadBillPayment/screens/BillDetailsScreen.tsx
+++ src/features/SadadBillPayment/screens/BillDetailsScreen.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { ViewStyle } from "react-native/types";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -5,6 +6,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import Button from "@/components/Button";
 import ContentContainer from "@/components/ContentContainer";
 import NavHeader from "@/components/NavHeader";
+import NotificationModal from "@/components/NotificationModal";
 import Page from "@/components/Page";
 import Stack from "@/components/Stack";
 import { useThemeStyles } from "@/theme";
@@ -15,10 +17,26 @@ import { billDetailsMock } from "../mocks/billDetailsMock";
 export default function BillDetailsScreen() {
   const { t } = useTranslation();
 
+  const [isConfirmDeleteModalVisible, setIsConfirmDeleteModalVisible] = useState(false);
+  const [isDeleteSuccessModalVisible, setIsDeleteSuccessModalVisible] = useState(false);
+  const [isDeleteErrorModalVisible, setIsDeleteErrorModalVisible] = useState(false);
+
   const buttonsContainerStyle = useThemeStyles<ViewStyle>(theme => ({
     marginTop: theme.spacing["24p"],
     width: "100%",
   }));
+
+  const handleOnBillDelete = () => {
+    setIsConfirmDeleteModalVisible(false);
+    try {
+      // TODO: this timeout is here just to mimic api call behavior. It will be replaced with API call in future.
+      setTimeout(() => {
+        setIsDeleteSuccessModalVisible(true);
+      }, 1000);
+    } catch (error) {
+      setIsDeleteErrorModalVisible(true);
+    }
+  };
 
   return (
     <SafeAreaProvider>
@@ -31,11 +49,60 @@ export default function BillDetailsScreen() {
               <Button variant="primary" disabled={billDetailsMock.paidAmount >= billDetailsMock.billAmount}>
                 {t("SadadBillPayments.BillDetailsScreen.payBillButton")}
               </Button>
-              <Button variant="tertiary">{t("SadadBillPayments.BillDetailsScreen.deleteBillButton")}</Button>
+              <Button variant="tertiary" onPress={() => setIsConfirmDeleteModalVisible(true)}>
+                {t("SadadBillPayments.BillDetailsScreen.deleteBillButton")}
+              </Button>
             </Stack>
           </Stack>
         </ContentContainer>
       </Page>
+      {/* Delete confirmation modal */}
+      <NotificationModal
+        variant="warning"
+        buttons={{
+          primary: (
+            <Button onPress={handleOnBillDelete}>
+              {t("SadadBillPayments.BillDetailsScreen.deleteBill.deleteButton")}
+            </Button>
+          ),
+          secondary: (
+            <Button onPress={() => setIsConfirmDeleteModalVisible(false)}>
+              {t("SadadBillPayments.BillDetailsScreen.deleteBill.cancelButton")}
+            </Button>
+          ),
+        }}
+        message={t("SadadBillPayments.BillDetailsScreen.deleteBill.message")}
+        title={t("SadadBillPayments.BillDetailsScreen.deleteBill.title")}
+        isVisible={isConfirmDeleteModalVisible}
+      />
+      {/* Delete success modal */}
+      <NotificationModal
+        variant="success"
+        buttons={{
+          primary: (
+            <Button onPress={() => setIsDeleteSuccessModalVisible(false)}>
+              {t("SadadBillPayments.BillDetailsScreen.deleteBill.okButtonText")}
+            </Button>
+          ),
+        }}
+        message={t("SadadBillPayments.BillDetailsScreen.deleteBill.deleteSuccess.message")}
+        title={t("SadadBillPayments.BillDetailsScreen.deleteBill.deleteSuccess.title")}
+        isVisible={isDeleteSuccessModalVisible}
+      />
+      {/* Delete error modal */}
+      <NotificationModal
+        variant="error"
+        buttons={{
+          primary: (
+            <Button onPress={() => setIsDeleteErrorModalVisible(false)}>
+              {t("SadadBillPayments.BillDetailsScreen.deleteBill.okButtonText")}
+            </Button>
+          ),
+        }}
+        message={t("SadadBillPayments.BillDetailsScreen.deleteBill.deleteError.message")}
+        title={t("SadadBillPayments.BillDetailsScreen.deleteBill.deleteError.title")}
+        isVisible={isDeleteErrorModalVisible}
+      />
     </SafeAreaProvider>
   );
 }
