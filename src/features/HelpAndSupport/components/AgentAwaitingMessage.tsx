3e6a57ac6214b40a4cef3bc039110d347492afdd
Ahmad Marie <amarei@ejada.com>
2023-08-17 13:24:10 +0000

    Pull request #1083: Feat/PC-15316 agent awaiting message
    Merge in MOB/croatia-mobile-app from feat/PC-15316-agent-awaiting-message to main
    
    Squashed commit of the following:
    
    commit 42b277d6b4bc864a26686fb555cd10c63ab6dabb
    Merge: 29953565e 0c14df287
    Author: Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
    Date:   Thu Aug 17 16:05:26 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into feat/PC-15316-agent-awaiting-message
    
    commit 29953565e44eeb0744649c9a023d26dfc68ca173
    Author: Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
    Date:   Thu Aug 17 14:07:57 2023 +0300
    
        fix: :zap: some update about style
    
    commit db86eb7cc94df2b99b2a5eb4390d572979ca5764
    Author: Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
    Date:   Thu Aug 17 13:57:02 2023 +0300
    
        fix: :bug: update language file
    
    commit c5f7bc093c4d043ff6c94dbe95a8f17aefbe8086
    Merge: 1c77dc7a6 e800ba108
    Author: Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
    Date:   Thu Aug 17 13:23:13 2023 +0300
    
        Merge branch 'main' into feat/PC-15316-agent-awaiting-message
    
    commit 1c77dc7a6a67c4d25779e0649cb4f1ff4173f807
    Merge: 150ceeb6a 63662afa4
    Author: Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
    Date:   Thu Aug 17 13:13:24 2023 +0300
    
        Merge branch 'main' into feat/PC-15316-agent-awaiting-message
    
    commit 150ceeb6a99a2579ec9a40c5df731c18b62d2e6b
    Author: Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
    Date:   Thu Aug 17 12:45:22 2023 +0300
    
        fix: :art: format the code
    
    commit a26cbf0d76562da9846d5aff6d27615bb511e8ab
    Author: Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
    Date:   Wed Aug 16 16:08:09 2023 +0300
    
        fix: :zap: add translation
    
    commit 4587b08ed0590c424f413619e2b6eb7032bbe323
    Author: Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
    Date:   Wed Aug 16 14:34:31 2023 +0300
    
        feat: :triangular_flag_on_post: waiting message

---
+++ src/features/HelpAndSupport/components/AgentAwaitingMessage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { View, ViewStyle } from "react-native";
+
+import Typography from "@/components/Typography";
+import { useThemeStyles } from "@/theme";
+
+const AgentAwaitingMessage = () => {
+  const { t } = useTranslation();
+
+  const container = useThemeStyles<ViewStyle>(theme => ({
+    borderColor: theme.palette["primaryBase-40"],
+    borderStartWidth: 4,
+    borderTopRightRadius: theme.radii.extraSmall,
+    borderBottomRightRadius: theme.radii.extraSmall,
+    padding: theme.spacing["16p"],
+    backgroundColor: theme.palette["supportBase-20"],
+  }));
+
+  return (
+    <View style={container}>
+      <Typography.Text size="caption2">{t("HelpAndSupport.LiveChatScreen.waitingMessage")}</Typography.Text>
+    </View>
+  );
+};
+
+export default AgentAwaitingMessage;
