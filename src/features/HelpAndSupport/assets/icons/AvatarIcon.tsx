e800ba1080453b02c03c0f8271daf95d18251e9d
Huthaifa Bani Hamad <hbanihamad@ejada.com>
2023-08-17 10:15:58 +0000

    Pull request #1085: Feat/(pc 15316)  create custom header
    Merge in MOB/croatia-mobile-app from feat/(pc-15316)--create-custom-header to main
    
    Squashed commit of the following:
    
    commit 921c2799b737ffd7f99d34c2ae1a903f1580fa2f
    Author: Huthaifa Bani Hamad <hbanihamad@ejada.com>
    Date:   Thu Aug 17 12:55:32 2023 +0300
    
        feat: fix conflicts
    
    commit ae1c654ef23667983f34106597c3d5673a1f4a42
    Merge: 5e87da3f8 63662afa4
    Author: Huthaifa Bani Hamad <hbanihamad@ejada.com>
    Date:   Thu Aug 17 12:49:37 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into feat/(pc-15316)--create-custom-header
    
    commit 5e87da3f8a8f5ad7064b30ba8e8b625a580834aa
    Author: Huthaifa Bani Hamad <hbanihamad@ejada.com>
    Date:   Thu Aug 17 12:28:11 2023 +0300
    
        feat: fix style
    
    commit e5e229ab2721414b379980773b23306b1281a405
    Author: Huthaifa Bani Hamad <hbanihamad@ejada.com>
    Date:   Thu Aug 17 12:06:59 2023 +0300
    
        feat: heder chat
    
    commit cd98aa7fa41c001d06e1eb8ba8ee47423f875803
    Merge: 29bed6700 03ba413cf
    Author: Huthaifa Bani Hamad <hbanihamad@ejada.com>
    Date:   Wed Aug 16 13:03:11 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into feat/(pc-15316)--create-custom-header
    
    commit 29bed67005aa92bbdf11dcca1ddfd67a3c89ee0d
    Author: Huthaifa Bani Hamad <hbanihamad@ejada.com>
    Date:   Wed Aug 16 13:02:23 2023 +0300
    
        feat: fix change
    
    commit 8010014470b13c1483eceeea8968d368bbb100d4
    Author: Huthaifa Bani Hamad <hbanihamad@ejada.com>
    Date:   Tue Aug 15 17:13:13 2023 +0300
    
        feat: fix
    
    commit 5a6f1a5c65f4b30159c2f7fa1606cc8ed20275e8
    Author: Huthaifa Bani Hamad <hbanihamad@ejada.com>
    Date:   Tue Aug 15 16:54:32 2023 +0300
    
        feat: change design
    
    commit df1c03e5fe35b6d869dba154c314171b147cf295
    Merge: 62e1c950b ffb5b9b8a
    Author: Huthaifa Bani Hamad <hbanihamad@ejada.com>
    Date:   Tue Aug 15 15:48:42 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into feat/(pc-15316)--create-custom-header
    
    commit 62e1c950baf42487c1382ef6cd1a35fd9d56b962
    Author: Huthaifa Bani Hamad <hbanihamad@ejada.com>
    Date:   Tue Aug 15 15:48:28 2023 +0300
    
        feat: add agent header info

---
+++ src/features/HelpAndSupport/assets/icons/AvatarIcon.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import Svg, { Defs, G, Path, Rect } from "react-native-svg";
+
+function AvatarIcon() {
+  return (
+    <Svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <Rect x="0.5" y="0.5" width="31" height="31" rx="15.5" fill="white" />
+      <Rect x="0.5" y="0.5" width="31" height="31" rx="15.5" stroke="#FF371E" />
+      <G clip-path="url(#clip0_2871_12267)">
+        <Path
+          d="M21.2445 10.6415L21.9988 10.2121V21.789L21.2445 21.3574L18.1217 19.5735V23.9989L8 18.2167L8.76017 17.785L11.8807 16.0011L18.1228 19.5678V12.4299L11.8783 15.9989L8.76017 14.2184L8 13.7845L18.1228 8V12.4254L21.2456 10.6415H21.2445Z"
+          fill="#FF371E"
+        />
+      </G>
+      <Defs>
+        <clipPath id="clip0_2871_12267">
+          <Rect width="14" height="16" fill="white" transform="translate(8 8)" />
+        </clipPath>
+      </Defs>
+    </Svg>
+  );
+}
+
+export default AvatarIcon;
---
+++ src/features/HelpAndSupport/components/AgentInformation.tsx
@@ -0,0 +1,74 @@
+import { useTranslation } from "react-i18next";
+import { StyleSheet, View, ViewStyle } from "react-native";
+
+import Typography from "@/components/Typography";
+import { useThemeStyles } from "@/theme";
+
+import { AvatarIcon } from "../assets/icons";
+
+interface AgentInformationProps {
+  isOnline: boolean;
+}
+
+export default function AgentInformation({ isOnline }: AgentInformationProps) {
+  const { t } = useTranslation();
+
+  const containerStyle = useThemeStyles<ViewStyle>(theme => ({
+    alignItems: "center",
+    backgroundColor: theme.palette["neutralBase-50"],
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: theme.spacing["16p"],
+    height: theme.spacing["64p"],
+    gap: theme.spacing["4p"],
+  }));
+
+  const contentContainerStyle = useThemeStyles<ViewStyle>(theme => ({
+    flexDirection: "column",
+    paddingHorizontal: theme.spacing["8p"],
+  }));
+
+  const statusCircleStyle = useThemeStyles<ViewStyle>(theme => ({
+    borderRadius: theme.radii.medium,
+    height: theme.spacing["8p"],
+    width: theme.spacing["8p"],
+    marginRight: theme.spacing["4p"],
+  }));
+
+  const iconAvailableColor = useThemeStyles(theme => theme.palette.secondary_mintBase);
+  const iconOfflineColor = useThemeStyles(theme => theme.palette["neutralBase+10"]);
+
+  return (
+    <View style={containerStyle}>
+      <View style={styles.agentInfo}>
+        <AvatarIcon />
+        <View style={contentContainerStyle}>
+          <Typography.Text weight="medium" color="neutralBase+30" size="callout">
+            {t("HelpAndSupport.AgentInformation.agentName")}
+          </Typography.Text>
+
+          <Typography.Text weight="regular" color="neutralBase+10" size="caption1">
+            {t("HelpAndSupport.AgentInformation.position")}
+          </Typography.Text>
+        </View>
+      </View>
+      <View style={styles.statusContainer}>
+        <View style={[statusCircleStyle, { backgroundColor: isOnline ? iconAvailableColor : iconOfflineColor }]} />
+        <Typography.Text weight="medium" color={isOnline ? "secondary_mintBase" : "neutralBase+10"} size="callout">
+          {isOnline ? t("HelpAndSupport.AgentInformation.online") : t("HelpAndSupport.AgentInformation.offline")}
+        </Typography.Text>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  agentInfo: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
+
+  statusContainer: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
+});
