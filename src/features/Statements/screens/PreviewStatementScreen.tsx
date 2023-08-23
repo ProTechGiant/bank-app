b0678f0423c72d8d5dcdc7513b03a42cad6eb045
Kalsoom Irshad <kalsoom-v@ejada.com>
2023-08-23 12:02:31 +0000

    Pull request #1124: fix: removed mock data
    Merge in MOB/croatia-mobile-app from fix-removed-dummy-data- to main
    
    Squashed commit of the following:
    
    commit 81573bb6d36f647c46be74abf5edda2997056232
    Author: Kalsoom Irshad <kalsoom-v@ejada.com>
    Date:   Wed Aug 23 16:46:25 2023 +0500
    
        fix: removed unused import
    
    commit eaa5e6315d5ba48daf8778ef42214a2570b2cac6
    Author: Kalsoom Irshad <kalsoom-v@ejada.com>
    Date:   Wed Aug 23 16:36:46 2023 +0500
    
        fix: added check
    
    commit b77aa35f89e30ca5b4e0d3cf27c742d4e9f14cdb
    Author: Kalsoom Irshad <kalsoom-v@ejada.com>
    Date:   Wed Aug 23 16:16:45 2023 +0500
    
        fix: pr comments
    
    commit 2d133a74523a5413c27b8c8f4b4c50e6b950f39c
    Author: Kalsoom Irshad <kalsoom-v@ejada.com>
    Date:   Wed Aug 23 15:10:22 2023 +0500
    
        fix: hide button when loading and when no data to show
    
    commit de899eeccc5fe64980841de0674ac8f708792efa
    Author: Kalsoom Irshad <kalsoom-v@ejada.com>
    Date:   Wed Aug 23 15:01:52 2023 +0500
    
        fix: renamed constant
    
    commit 9db19500bc39a46f8e2c82ab169103ebbb5f041c
    Author: Kalsoom Irshad <kalsoom-v@ejada.com>
    Date:   Wed Aug 23 14:50:33 2023 +0500
    
        fix: removed dummy data

--- src/features/Statements/screens/PreviewStatementScreen.tsx
+++ src/features/Statements/screens/PreviewStatementScreen.tsx
@@ -1,7 +1,7 @@
 import { RouteProp, useRoute } from "@react-navigation/native";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Pressable, View, ViewStyle } from "react-native";
+import { Pressable, StyleSheet, View, ViewStyle } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Share, { ShareOptions } from "react-native-share";
@@ -9,14 +9,16 @@ import Share, { ShareOptions } from "react-native-share";
 import { CloseIcon, FullScreenIcon } from "@/assets/icons";
 import Button from "@/components/Button";
 import ContentContainer from "@/components/ContentContainer";
+import FullScreenLoader from "@/components/FullScreenLoader";
 import NavHeader from "@/components/NavHeader";
 import Page from "@/components/Page";
+import Typography from "@/components/Typography";
 import { warn } from "@/logger";
 import { useThemeStyles } from "@/theme";
 
 import { PreviewStatementPDF } from "../components";
 import { useDownloadStatement } from "../hooks/query-hooks";
-import { STATEMENT_BASE_64_STRING } from "../mocks/AccessStatementData";
+import { STATEMENT_BASE_64_PREFIX } from "../mocks/AccessStatementData";
 import { StatementsStackParams } from "../StatementsStack";
 
 export default function PreviewStatementScreen() {
@@ -24,21 +26,22 @@ export default function PreviewStatementScreen() {
   const route = useRoute<RouteProp<StatementsStackParams, "Statements.PreviewStatementScreen">>();
   const documentId = route.params?.documentId;
 
-  const { data } = useDownloadStatement(documentId);
+  const { data, isLoading } = useDownloadStatement(documentId);
   const { t } = useTranslation();
 
   const handleExportStatement = async () => {
-    const shareOptions: ShareOptions = {
-      title: "Statement",
-      //TODO: will change it to url once backend is ready
-      url: data?.StatementContent || STATEMENT_BASE_64_STRING,
-    };
+    if (data !== undefined) {
+      const shareOptions: ShareOptions = {
+        title: "Statement",
+        url: STATEMENT_BASE_64_PREFIX + data.StatementContent,
+      };
 
-    try {
-      const ShareResponse = await Share.open(shareOptions);
-      warn("Result =>", JSON.stringify(ShareResponse));
-    } catch (error) {
-      warn("sharePdfBase64 Error =>", JSON.stringify(error));
+      try {
+        const ShareResponse = await Share.open(shareOptions);
+        warn("Result =>", JSON.stringify(ShareResponse));
+      } catch (error) {
+        warn("sharePdfBase64 Error =>", JSON.stringify(error));
+      }
     }
   };
 
@@ -77,13 +80,21 @@ export default function PreviewStatementScreen() {
         </SafeAreaView>
 
         <ContentContainer>
-          {/* {data?.StatementContent !== undefined ? ( TODO: will uncomment once api starts working*/}
-          <PreviewStatementPDF
-            fullPreviewMode={fullPreviewMode}
-            source={{ uri: data?.StatementContent || STATEMENT_BASE_64_STRING }} //TODO: Will remove fallback when API will be available
-          />
-          {/* ) : null} */}
-          {!fullPreviewMode ? (
+          {isLoading ? (
+            <FullScreenLoader />
+          ) : data?.StatementContent === undefined ? (
+            <ContentContainer style={styles.container}>
+              <Typography.Text weight="regular" size="title3" align="center">
+                {t("Statements.PreviewStatementScreen.nothingToShowText")}
+              </Typography.Text>
+            </ContentContainer>
+          ) : (
+            <PreviewStatementPDF
+              fullPreviewMode={fullPreviewMode}
+              source={{ uri: STATEMENT_BASE_64_PREFIX + data.StatementContent }}
+            />
+          )}
+          {data?.StatementContent && !isLoading && !fullPreviewMode ? (
             <View style={bottomButtonContainerStyle}>
               <Button onPress={handleExportStatement}>{t("Statements.PreviewStatementScreen.exportAsPDF")}</Button>
             </View>
@@ -93,3 +104,10 @@ export default function PreviewStatementScreen() {
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
