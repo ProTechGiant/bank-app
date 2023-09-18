25dae66ea3837bb822fd5c6ac6c6c84d56c34f12
Rafeh Atique <rafeh-v@ejada.com>
2023-09-18 09:33:15 +0000

    Pull request #1252: feat(PC-17667): AC3 Auto Lockout after a 5 minute period of inactivity
    Merge in MOB/croatia-mobile-app from feat/PC-17667-lockout-user-after-inactivity to main
    
    Squashed commit of the following:
    
    commit 2d0818f78f6648266221b0fa19beab8f467c6278
    Author: Rafeh Atique <rafeh-v@ejada.com>
    Date:   Mon Sep 18 14:25:21 2023 +0500
    
        chore: added yarn changes
    
    commit 57288ef09039ea340d2fc8798fa541612b9f64d0
    Author: Rafeh Atique <rafeh-v@ejada.com>
    Date:   Wed Sep 13 18:24:11 2023 +0500
    
        fix: moved comment
    
    commit 20c69040644cda0d82a6320483865fc58481f4db
    Author: Rafeh Atique <rafeh-v@ejada.com>
    Date:   Wed Sep 13 17:58:54 2023 +0500
    
        feat: done with the inactivity functionality

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -1,4 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet } from "react-native";
+import UserInactivity from "react-native-user-inactivity";
 
 import { useAuthContext } from "@/contexts/AuthContext";
 import { useDeepLinkHandler } from "@/hooks/use-deeplink-handler";
@@ -6,18 +8,36 @@ import { navigationRef } from "@/navigation/NavigationService";
 
 import { AuthenticatedScreens } from "./AuthenticatedStack";
 import { UnauthenticatedScreens } from "./UnAuthenticatedStack";
-
 interface MainStackProps {
   onReady: () => void;
 }
+const TIMEOUT_MS = 300000; // 5 minutes
 
 export default function MainStack({ onReady }: MainStackProps) {
-  const { isAuthenticated } = useAuthContext();
-  useDeepLinkHandler();
+  const { isAuthenticated, logout } = useAuthContext();
 
+  useDeepLinkHandler();
   return (
     <NavigationContainer onReady={onReady} ref={navigationRef}>
-      {isAuthenticated ? <AuthenticatedScreens /> : <UnauthenticatedScreens />}
+      {isAuthenticated ? (
+        <UserInactivity
+          isActive={true}
+          timeForInactivity={TIMEOUT_MS}
+          onAction={isActive => {
+            if (!isActive) {
+              logout(true);
+            }
+          }}
+          style={styles.userInactivityContainer}>
+          <AuthenticatedScreens />
+        </UserInactivity>
+      ) : (
+        <UnauthenticatedScreens />
+      )}
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  userInactivityContainer: { flex: 1 },
+});
