ce80fdcb2e5b15c31d9dd411ceaa4947c2be4b45
Sian Finlay <svfinlay@deloitte.co.uk>
2023-09-21 11:06:08 +0000

    Pull request #1300: fix(PC-21109): Animation stops when you leave the app
    Merge in MOB/croatia-mobile-app from fix/animation-stops-when-you-leave-the-app to main
    
    Squashed commit of the following:
    
    commit b0ab00fc70a714cc9edcc429642b56621c73e10f
    Author: Sian Finlay <svfinlay@deloitte.co.uk>
    Date:   Wed Sep 20 15:32:57 2023 +0100
    
        fix: animation resumes on reentering to app

--- src/components/AnimationView/index.tsx
+++ src/components/AnimationView/index.tsx
@@ -1,4 +1,5 @@
 import LottieView, { AnimationObject } from "lottie-react-native";
+import { useRef } from "react";
 import { StyleProp, StyleSheet, View, ViewStyle } from "react-native";
 
 interface AnimationViewProps {
@@ -9,9 +10,25 @@ interface AnimationViewProps {
 }
 
 export default function AnimationView({ source, speed = 1, autoplay = true, style }: AnimationViewProps) {
+  const animationRef = useRef<LottieView>(null);
+
+  const playAnimation = () => {
+    animationRef.current?.play();
+  };
+
   return (
     <View style={styles.container}>
-      <LottieView source={source} autoPlay={autoplay} speed={speed} style={[styles.lottie, style]} />
+      <LottieView
+        ref={animationRef}
+        source={source}
+        autoPlay={autoplay}
+        speed={speed}
+        onAnimationFinish={() => {
+          // if user leaves app, the animation will resume playing when they return instead of ending the animation.
+          playAnimation();
+        }}
+        style={[styles.lottie, style]}
+      />
     </View>
   );
 }
