4d98fd4e24fe62aff5d3511119210be037e58fbe
Khaled Alaa Eldin <kalaaeldin@ejada.com>
2023-10-04 15:12:29 +0000

    Pull request #1413: fix: remove mock logout
    Merge in MOB/croatia-mobile-app from fix/remove-logout--mock to main
    
    Squashed commit of the following:
    
    commit 4a6417edd9c7d19e38d0e73e1af059dc1b0e48f6
    Author: kalaaeldin@ejada.com <kalaaeldin@ejada.com>
    Date:   Wed Oct 4 16:40:17 2023 +0300
    
        fix: remove mock logout

--- src/hooks/use-logout.ts
+++ src/hooks/use-logout.ts
@@ -1,8 +1,12 @@
+import DeviceInfo from "react-native-device-info";
 import { useMutation } from "react-query";
 
 import sendApiRequest from "@/api";
 import { useAuthContext } from "@/contexts/AuthContext";
 import { generateRandomId } from "@/utils";
+import { setItemInEncryptedStorage } from "@/utils/encrypted-storage";
+
+import { useGetAuthenticationToken } from "./use-api-authentication-token";
 
 export enum logoutActionsIds {
   MANUALLY_ID = 1,
@@ -11,29 +15,33 @@ export enum logoutActionsIds {
 
 export default function useLogout() {
   const auth = useAuthContext();
-  const logoutAsync = useMutation(async (ActionId: number) => {
-    //TODO: will remove when API is ready
-    const testMode = true;
-    if (testMode) {
-      return {
-        status: 200,
-      };
-    } else {
-      return sendApiRequest<boolean>(
-        "v1",
-        "customers/sign-out",
-        "PATCH",
-        undefined,
-        { ActionId: ActionId },
-        {
-          ["x-correlation-id"]: generateRandomId(),
-        }
-      );
+  const { mutateAsync: getAuthenticationToken } = useGetAuthenticationToken();
+
+  const handleGetAuthenticationToken = async () => {
+    const res = await getAuthenticationToken();
+    if (typeof res?.AccessToken === "string") {
+      setItemInEncryptedStorage("authToken", res.AccessToken);
+      auth.authenticateAnonymously(auth.userId as string, res.AccessToken);
     }
+  };
+
+  const logoutAsync = useMutation(async (ActionId: number) => {
+    return sendApiRequest<boolean>(
+      "v2",
+      "customers/sign-out",
+      "PATCH",
+      undefined,
+      { ActionId: ActionId },
+      {
+        ["x-correlation-id"]: generateRandomId(),
+        ["x-device-name"]: await DeviceInfo.getDeviceName(),
+      }
+    );
   });
 
   return async function (actionId: number) {
     await logoutAsync.mutateAsync(actionId);
     auth.logout(actionId === logoutActionsIds.AUTOMATIC_ID);
+    handleGetAuthenticationToken();
   };
 }
