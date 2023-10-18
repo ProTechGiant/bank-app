8e77a7cb618c7da79cc8b1126968dfdd1e46c324
Rafeh Atique <rafeh-v@ejada.com>
2023-10-18 06:36:33 +0000

    Pull request #1463: Feat(PC-22014): Updated information
    Merge in MOB/croatia-mobile-app from feat/update-account-info to main
    
    Squashed commit of the following:
    
    commit 22337e6baac14cb81897c2662d8032a7e45dce9f
    Author: Rafeh Atique <rafeh-v@ejada.com>
    Date:   Mon Oct 16 16:29:21 2023 +0500
    
        feat: updated the data
    
    commit e4ca95e88429518a91588bdb0bccfee3ca8fe290
    Author: Rafeh Atique <rafeh-v@ejada.com>
    Date:   Mon Oct 16 12:19:35 2023 +0500
    
        chore: updated the data
    
    commit 4aedaa1c041bd420540337bdd716a1f2fae410cc
    Author: Rafeh Atique <rafeh-v@ejada.com>
    Date:   Wed Oct 11 18:14:03 2023 +0500
    
        feat: done with updating the mock

--- src/mocks/expectedAmount.ts
+++ src/mocks/expectedAmount.ts
@@ -1,14 +1,15 @@
 export const mockExpectedAmount = [
-  {
-    label: "SAR 1000 – 5000",
-    value: "SAR 1000 – 5000",
-  },
-  {
-    label: "SAR 5000 – 10000",
-    value: "SAR 5000 – 10000",
-  },
-  {
-    label: "SAR 10000 – 20000",
-    value: "SAR 10000 – 20000",
-  },
+  // value is a code
+  { label: "Less Than 3000", value: "1" },
+  { label: "From 3001 to 6000", value: "2" },
+  { label: "From 6001 to 10000", value: "3" },
+  { label: "From 10001 to 15000", value: "4" },
+  { label: "From 15001 to 20000", value: "8" },
+  { label: "From 20001 to 30000", value: "5" },
+  { label: "From 30001 to 40000", value: "9" },
+  { label: "From 40001 to 60000", value: "6" },
+  { label: "From 50000 to 60000", value: "C" },
+  { label: "From 60001 to 80000", value: "A" },
+  { label: "From 80001 to 100000", value: "7" },
+  { label: "More than 100000", value: "B" },
 ];
