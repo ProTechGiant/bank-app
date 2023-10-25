4085c8a70c273268cadd81716431f81705738574
Ra’ed Khwayreh <rkhwayreh@ejada.com>
2023-10-25 22:00:34 +0000

    Pull request #1528: Feat/PC-22844 Goal Landing Page
    Merge in MOB/croatia-mobile-app from feat/PC-22844-Goal-Landing-Page to main
    
    Squashed commit of the following:
    
    commit cb8d5c0c587a4803559cc113fe393886da9d5c35
    Merge: 2c8578f61 ce65488cb
    Author: Raed-Khwayreh <78206754+Raed-Khwayreh@users.noreply.github.com>
    Date:   Wed Oct 25 17:12:08 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into feat/PC-22844-Goal-Landing-Page
    
    commit 2c8578f61acb1723871c787a3852ea87a10d1b43
    Merge: a858bf613 b22474d2c
    Author: Raed-Khwayreh <78206754+Raed-Khwayreh@users.noreply.github.com>
    Date:   Wed Oct 25 10:16:33 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into feat/PC-22844-Goal-Landing-Page
    
    commit a858bf61337b0a90ad52a7ea9a1ad956bf81d927
    Author: Raed-Khwayreh <78206754+Raed-Khwayreh@users.noreply.github.com>
    Date:   Wed Oct 25 10:15:38 2023 +0300
    
        fix: use button
    
    commit 9551c62ff275b919075e6ee839a2373eae38c32a
    Merge: 8d47afc21 fb1c7c28b
    Author: Huthaifa Bani Hamad <hbanihamad@ejada.com>
    Date:   Tue Oct 24 23:25:24 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into feat/PC-22844-Goal-Landing-Page
    
    commit 8d47afc21f62b6c8bf936761a33f3d79129e17b9
    Merge: b63715f86 76dfdb7e8
    Author: Raed-Khwayreh <78206754+Raed-Khwayreh@users.noreply.github.com>
    Date:   Tue Oct 24 15:15:55 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into feat/PC-22844-Goal-Landing-Page
    
    commit b63715f867657ebbf564b5a3fc78dfd3a80a70f6
    Merge: 63f00a253 da04ac8d9
    Author: Raed-Khwayreh <78206754+Raed-Khwayreh@users.noreply.github.com>
    Date:   Tue Oct 24 13:21:00 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into feat/PC-22844-Goal-Landing-Page
    
    commit 63f00a2535995676d9ddde6e9b597e8863a24d6d
    Author: Raed-Khwayreh <78206754+Raed-Khwayreh@users.noreply.github.com>
    Date:   Tue Oct 24 13:20:05 2023 +0300
    
        fix: translation
    
    commit b16bf4ed92fe553e1b37733e5a2b9e9bb92dbd04
    Author: Raed-Khwayreh <78206754+Raed-Khwayreh@users.noreply.github.com>
    Date:   Tue Oct 24 12:02:03 2023 +0300
    
        fix: wrong fun. name
    
    commit a538c58fef2fce5f74eba71dbde4cebd53e64198
    Merge: 9e4ced9e6 bcd97911a
    Author: Raed-Khwayreh <78206754+Raed-Khwayreh@users.noreply.github.com>
    Date:   Tue Oct 24 11:56:54 2023 +0300
    
        Merge branch 'main' of https://bitbucket.projectcroatia.cloud/scm/mob/croatia-mobile-app into feat/PC-22844-Goal-Landing-Page
    
    commit 9e4ced9e64100e88d403e34c7923ea6274dde94b
    Author: Raed-Khwayreh <78206754+Raed-Khwayreh@users.noreply.github.com>
    Date:   Tue Oct 24 11:54:10 2023 +0300
    
        feat: implement Goal Getter landing page

--- src/features/GoalGetter/screens/GoalGetterScreen.tsx
+++ src/features/GoalGetter/screens/GoalGetterScreen.tsx
@@ -1,89 +1,88 @@
-import { ReactNode } from "react";
 import { useTranslation } from "react-i18next";
-import { StyleSheet, useWindowDimensions, View } from "react-native";
+import { StatusBar, useWindowDimensions, View, ViewStyle } from "react-native";
 
-import { HeroSlider } from "@/components/HeroSlider";
-import { HeroSlideProps } from "@/components/HeroSlider/HeroSlide";
+import { CloseIcon } from "@/assets/icons";
+import Button from "@/components/Button";
+import ContentContainer from "@/components/ContentContainer";
 import NavHeader from "@/components/NavHeader";
+import Page from "@/components/Page";
+import Stack from "@/components/Stack";
+import Typography from "@/components/Typography";
 import useNavigation from "@/navigation/use-navigation";
+import { useThemeStyles } from "@/theme";
 
-import LandingGoalGetter from "../assets/icons/LandingGoalGetter";
+import GoalGetterCreated from "../assets/goal-getter-created.svg";
 
 export default function GoalGetterScreen() {
-  const navigation = useNavigation();
   const { t } = useTranslation();
+  const navigation = useNavigation();
   const { height } = useWindowDimensions();
 
-  const svgHeight = height * 0.5;
-  const svgWidth = svgHeight * 0.8;
-  const svgStyles = { top: height * 0.52 };
-
-  const handleOnFinish = () => {
-    navigation.navigate("GoalGetter.GoalMindScreen");
+  const handleOnPressClose = () => {
+    navigation.navigate("Home.HomeTabs");
   };
 
-  const handleOnBack = () => {
-    navigation.goBack();
+  const handleOnPressCreateGoal = () => {
+    //TODO: redirected to stage 1 of create goal page
   };
 
-  const createHeroSlide = (topElement: ReactNode, title: string, text: string) => {
-    return {
-      topElement: <View style={styles.topElementStyle}>{topElement}</View>,
-      title: title,
-      text: text,
-      containerStyle: styles.containerStyle,
-      bottomElementStyle: [styles.bottomElementStyle, svgStyles],
-    };
+  const handleOnPressExploreProducts = () => {
+    //TODO: redirected to Investment Dashboard screen
   };
 
-  const data: HeroSlideProps[] = [
-    createHeroSlide(
-      <LandingGoalGetter height={svgHeight} width={svgWidth} />,
-      t("GoalGetter.landingScreen.titleOne"),
-      t("GoalGetter.landingScreen.subTextOne")
-    ),
-    createHeroSlide(
-      <LandingGoalGetter height={svgHeight} width={svgWidth} />,
-      t("GoalGetter.landingScreen.titleTwo"),
-      t("GoalGetter.landingScreen.subTextTwo")
-    ),
-    createHeroSlide(
-      <LandingGoalGetter height={svgHeight} width={svgWidth} />,
-      t("GoalGetter.landingScreen.titleThree"),
-      t("GoalGetter.landingScreen.subTextThree")
-    ),
-    createHeroSlide(
-      <LandingGoalGetter height={svgHeight} width={svgWidth} />,
-      t("GoalGetter.landingScreen.titleFour"),
-      t("GoalGetter.landingScreen.subTextFour")
-    ),
-  ];
+  const headerSuccessStyle = useThemeStyles<ViewStyle>(theme => ({
+    alignItems: "center",
+    marginBottom: theme.spacing["16p"],
+  }));
+
+  const buttonContainerStyle = useThemeStyles<ViewStyle>(theme => ({
+    marginTop: theme.spacing["24p"],
+    paddingBottom: theme.spacing["20p"],
+  }));
+
+  const svgHeight = height * 0.4; // Adjust the height as needed
+  const svgWidth = svgHeight * 0.6; // Adjust the aspect ratio as needed
 
   return (
-    <HeroSlider
-      variant="default"
-      onFinishPress={handleOnFinish}
-      onBackPress={handleOnBack}
-      end={<NavHeader.TextEndButton onPress={handleOnFinish} text={t(`GoalGetter.landingScreen.skip`)} />}
-      buttonText={t("GoalGetter.landingScreen.next")}
-      lastButtonText={t("GoalGetter.landingScreen.createGoal")}
-      data={data}
-      title={t("GoalGetter.landingScreen.title")}
-    />
+    <Page backgroundColor="neutralBase-60">
+      <NavHeader
+        title={t("GoalGetter.GoalGetterScreen.title")}
+        withBackButton={false}
+        end={<NavHeader.IconEndButton icon={<CloseIcon />} onPress={handleOnPressClose} />}
+      />
+      <StatusBar backgroundColor="transparent" barStyle="dark-content" translucent />
+      <ContentContainer>
+        <Stack direction="vertical" flex={1} justify="space-between" gap="24p" align="stretch">
+          <View style={headerSuccessStyle}>
+            <GoalGetterCreated height={svgHeight} width={svgWidth} />
+            <Stack direction="vertical" align="center" gap="12p">
+              <Typography.Text align="center" size="body" weight="bold" color="neutralBase+30">
+                {t("GoalGetter.GoalGetterScreen.whereToNext")}
+              </Typography.Text>
+              <Typography.Text align="center" size="callout" weight="regular" color="neutralBase+10">
+                {t("GoalGetter.GoalGetterScreen.hopesAndDreams")}
+              </Typography.Text>
+            </Stack>
+          </View>
+        </Stack>
+        <Stack direction="vertical" align="stretch" gap="12p">
+          <Typography.Text align="center" size="large" weight="bold" color="neutralBase+30">
+            {t("GoalGetter.GoalGetterScreen.title")}
+          </Typography.Text>
+          <Stack gap="8p" direction="vertical" align="stretch" style={buttonContainerStyle}>
+            <Button onPress={handleOnPressCreateGoal}>
+              <Typography.Text align="center" size="body" weight="regular" color="neutralBase-60">
+                {t("GoalGetter.GoalGetterScreen.createGoalbutton")}
+              </Typography.Text>
+            </Button>
+            <Button variant="tertiary" onPress={handleOnPressExploreProducts}>
+              <Typography.Text align="center" size="body" weight="medium" color="neutralBase+30">
+                {t("GoalGetter.GoalGetterScreen.exploreProductsbutton")}
+              </Typography.Text>
+            </Button>
+          </Stack>
+        </Stack>
+      </ContentContainer>
+    </Page>
   );
 }
-const styles = StyleSheet.create({
-  bottomElementStyle: {
-    position: "absolute",
-    width: "100%",
-  },
-  containerStyle: {
-    alignItems: "center",
-    height: "100%",
-    justifyContent: "space-between",
-  },
-  topElementStyle: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
