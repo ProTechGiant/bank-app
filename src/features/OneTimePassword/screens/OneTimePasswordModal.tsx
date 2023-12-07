2d75cc86d3b24de2bd43904229b6c0babaadb947
Zohaib Shahid <zshaid-v@ejada.com>
2023-12-07 13:52:55 +0000

    Pull request #1911: FIX: Two different messages appear to customer after waiting timer to reach 0:00
    Merge in MOB/croatia-mobile-app from fix/PC-33110 to main
    
    Squashed commit of the following:
    
    commit 10457844ddb47199f30d31ba1f79c5c3d6fac06b
    Author: Zohaib Shahid <z-shaid@ejada.com>
    Date:   Thu Dec 7 18:00:07 2023 +0500
    
        feat: 2bl banner issue

--- src/features/OneTimePassword/screens/OneTimePasswordModal.tsx
+++ src/features/OneTimePassword/screens/OneTimePasswordModal.tsx
@@ -322,7 +322,7 @@ export default function OneTimePasswordModal<ParamsT extends object, OutputT ext
                   length={OTP_CODE_LENGTH}
                   value={currentValue}
                 />
-                {otpEnterNumberOfAttemptsLeft > 0 ? (
+                {!isOtpExpired && otpEnterNumberOfAttemptsLeft > 0 ? (
                   otpEnterNumberOfAttemptsLeft === 2 ? (
                     <Alert variant="error" message={t("OneTimePasswordModal.errors.otp2AttemptsLeft")} />
                   ) : otpEnterNumberOfAttemptsLeft === 1 ? (
