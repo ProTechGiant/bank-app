d0f12f24029b4c26a13b3eda57ec963ae04d472c
Anandhu M <anandhu-v@ejada.com>
2023-12-26 07:13:18 +0000

    Pull request #2038: feat: credentials change for NI
    Merge in MOB/croatia-mobile-app from feat/NI-config-changes to main
    
    Squashed commit of the following:
    
    commit 69bdc3ee8ba81ccc6e8c5719855c7324e365c6b7
    Author: anandhu-v@ejada.com <anandhu-v@ejada.com>
    Date:   Tue Dec 26 11:06:01 2023 +0530
    
        feat: credentials change for NI

--- src/hooks/use-token.ts
+++ src/hooks/use-token.ts
@@ -3,8 +3,8 @@ import { useMutation } from "react-query";
 import api from "@/api";
 import { generateRandomId } from "@/utils";
 
-const NI_CLIENT_SECRET = "Q3HVEbgpY3";
-const NI_CLIENT_ID = "xzzy7tvwvvqy9v4f26xszkb9";
+const NI_CLIENT_SECRET = "5uL8FWhOhO4O8kjPTuYscDAcSz72zzfX";
+const NI_CLIENT_ID = "XrcdG1F8IQoFMPdFlj6DYtThdMZyMGL5uXJB5F7owGKa4ZLf";
 const NI_GRANT_TYPE = "client_credentials";
 
 export interface GetTokenResponse {
