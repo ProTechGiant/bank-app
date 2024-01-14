848c2949df2805a89199073a16662f8f3677c7ed
Moustafa AbdelAzem <mabdelazem@ejada.com>
2024-01-14 08:45:42 +0000

    Pull request #2256: fix: fix: mobile regex
    Merge in MOB/croatia-mobile-app from fix/mobile-regex-to-main-branch to main
    
    Squashed commit of the following:
    
    commit ac80373ab16510bc7893c1c9ce28c6433dc49418
    Author: Moustafa Mohamed <moustafa@Moustafas-MacBook-Pro.local>
    Date:   Thu Jan 11 18:15:16 2024 +0300
    
        fix: fix: mobile regex

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,7 @@ import DeviceInfo from "react-native-device-info";
 
 import { countryCodes } from "@/mocks/countryListDataWithCodes";
 
-export const nationalIdRegEx = /^[\d\u0660-\u0669\u06F0-\u06F9]{10}$/;
+export const nationalIdRegEx = /^[1١2٢][0-9٠-٩]{9}$/;
 
 export const alphaNumericSpaceRegExp = /^[a-zA-Z0-9 ]+$/;
 
