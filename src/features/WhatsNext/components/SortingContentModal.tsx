1d7d3a2b5acc91a3312bb0925d4e8c5f5b028938
Ahmed AbdelAziz <asabdelaziz@ejada.com>
2024-01-21 10:36:12 +0000

    Pull request #2391: fix: remove apply button from
    Merge in MOB/croatia-mobile-app from fix/PC-40405-remove-apply-button-from-sort-whats-next to main
    
    Squashed commit of the following:
    
    commit 3e7212de0352b1fec6a154eb3b2ca02f10a79ac9
    Author: asabdelaziz <asabdelaziz@ejada.com>
    Date:   Sun Jan 21 11:26:54 2024 +0200
    
        fix: remove apply button from

--- src/features/WhatsNext/components/SortingContentModal.tsx
+++ src/features/WhatsNext/components/SortingContentModal.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { View, ViewStyle } from "react-native";
 
-import Button from "@/components/Button";
 import Modal from "@/components/Modal";
 import { RadioButton, RadioButtonGroup } from "@/components/RadioButton";
-import Typography from "@/components/Typography";
 import { useThemeStyles } from "@/theme";
 
 import { SORT_NEWEST, SORT_OLDEST } from "../constants";
@@ -20,31 +17,17 @@ interface SortingContentModalProps {
 export default function SortingContentModal({ onClose, isVisible, onChange, sortOrder }: SortingContentModalProps) {
   const { t } = useTranslation();
 
-  const [selected, setSelected] = useState<typeof SORT_NEWEST | typeof SORT_OLDEST>(sortOrder);
-  const isApplyButtonDisabled = sortOrder === selected;
-
   const modalStyle = useThemeStyles<ViewStyle>(theme => ({
     borderRadius: theme.radii.small,
-  }));
-
-  const buttonContainerStyle = useThemeStyles<ViewStyle>(theme => ({
-    padding: theme.spacing["16p"],
   }));
 
   return (
     <Modal visible={isVisible} style={modalStyle} onClose={onClose} headerText={t("WhatsNext.SortingContent.title")}>
       <View>
-        <RadioButtonGroup onPress={value => setSelected(value)} value={selected}>
+        <RadioButtonGroup onPress={value => onChange(value)} value={sortOrder}>
           <RadioButton label={t("WhatsNext.SortingContent.newest")} value={SORT_NEWEST} />
           <RadioButton label={t("WhatsNext.SortingContent.oldest")} value={SORT_OLDEST} />
         </RadioButtonGroup>
-      </View>
-      <View style={buttonContainerStyle}>
-        <Button onPress={() => onChange(selected)} disabled={isApplyButtonDisabled}>
-          <Typography.Text color={isApplyButtonDisabled ? "neutralBase-20" : "neutralBase-60"} size="footnote">
-            {t("WhatsNext.SortingContent.apply")}
-          </Typography.Text>
-        </Button>
       </View>
     </Modal>
   );
